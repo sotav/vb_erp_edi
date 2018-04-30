3a15cacab03316974fd0c763f001c76196e76f5f
sotav <sotav@akronis.gr>
2018-04-30 10:21:08 +0300

    Run

--- EDI-ERP/My Project/Resources.Designer.vb
+++ EDI-ERP/My Project/Resources.Designer.vb
@@ -89,15 +89,5 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property logo1() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("logo1", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
